85bf2ff515a98c7c8d30bc293521702465fe2954
quinnHuang <quinn.huang@oocl.com>
2018-07-19 09:05:34 +0800

    [Quinn] add pretty

--- src/component/TaskGroup.js
+++ src/component/TaskGroup.js
@@ -1,44 +1,45 @@
-import React,{Component} from 'react'
-import '../css/todo.css';
+import React, { Component } from "react";
+import "../css/todo.css";
 
 export default class Task extends Component {
   constructor(props) {
     super(props);
-    this.state={editable:false};
+    this.state = { editable: false };
   }
-  editTask=(e)=>{
+  editTask = e => {
     //this.setState({editable:true});
     console.log("editTask");
-  }
-  endEdit=(event)=>{
+  };
+  endEdit = event => {
     let keycode = event.keyCode;
-    if (keycode === '13') {
-      this.setState({editable:false});
+    if (keycode === "13") {
+      this.setState({ editable: false });
     }
-  }
-  checkTask=(event)=>{
-    const task=this.props.tasks.find(t=> {return t.id==event.target.id});
-    let prechecked=task.checked;
-    task.checked=!prechecked;
+  };
+  checkTask = event => {
+    const task = this.props.tasks.find(t => {
+      return t.id == event.target.id;
+    });
+    let prechecked = task.checked;
+    task.checked = !prechecked;
     this.props.check(this.props.tasks);
-  }
-  render(){
-    return(
+  };
+  render() {
+    return (
       <ol className="task">
-        {this.props.tasks.filter(t=>t.visual).map(task => (
-          <li className={task.checked?"checked":""}>
+        {this.props.tasks.filter(t => t.visual).map(task => (
+          <li className={task.checked ? "checked" : ""}>
             <input
               id={task.id}
               type="checkbox"
               className="done-todo"
-              checked={task.checked?"checked":""}
-              onClick={this.checkTask}/>
-            <span>
-              {task.text}
-            </span>
+              checked={task.checked ? "checked" : ""}
+              onClick={this.checkTask}
+            />
+            <span>{task.text}</span>
           </li>
         ))}
       </ol>
-    )
+    );
   }
 }
